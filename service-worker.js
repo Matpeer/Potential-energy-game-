3de50b976cb5ec8068e2876f70aa58ace140c909
Matpeer <matpeer@gmail.com>
2024-07-21 16:52:58 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+```javascript
+const CACHE_NAME = 'energy-game-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon-500x500.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+```
